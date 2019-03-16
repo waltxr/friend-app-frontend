57bcca529e635ed0e73cf2671fe01225a27da526
Adam Walter <Waltxr@users.noreply.github.com>
2019-03-16 16:17:56 -0400

    can search for users when in Dashboard.

--- src/container/users/grievanceForm.js
+++ src/container/users/grievanceForm.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react'
-import { Form } from 'semantic-ui-react'
+import { Form, Search, Grid, Header, Segment, Label } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 import { fileGrievance } from '../../actions/userActions'
+import _ from 'lodash'
+import faker from 'faker'
 
 class GrievanceForm extends Component {
   constructor(props){
     super(props)
       this.state = {
         title: "",
-        description: ""
+        description: "",
+        receiver_ids: [],
+        isLoading: false,
+        results: [],
+        value: ""
     }
   }
+
+  componentWillMount() {
+    this.resetComponent()
+  }
+
+  resetComponent = () => this.setState({ isLoading: false, results: [], value: '' })
+
+  handleResultSelect = (e, { result }) => this.setState({ value: result.name })
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value })
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.resetComponent()
+      console.log(this.state.value);
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const isMatch = result => re.test(result.name)
+
+      this.setState({
+        isLoading: false,
+        results: _.filter(this.props.users, isMatch),
+      })
+    }, 300)
+  }
+
 
   handleChange = (e) => {
     const {name, value} = e.target
@@ -32,7 +64,6 @@ class GrievanceForm extends Component {
     })
   }
 
-
   render() {
     console.log(this.state);
     const userOptions = this.props.users.map( user => {
@@ -42,18 +73,34 @@ class GrievanceForm extends Component {
         value: user.id
       }
     })
-
+    console.log(this.state.results);
+    const resultRenderer = ({ name }) => <Label content={name} />
 
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <span>File a Grievance:</span>
-        <Form.Group widths='equal'>
-          <Form.Select fluid onChange={this.handleUserChange} label='Receiver' name='receiver_ids' placeholder='Receiver' options={userOptions}/>
-        </Form.Group>
-        <Form.Input fluid label='Title' placeholder='Title' name='title' onChange={this.handleChange} value={this.state.title}/>
-        <Form.TextArea label='Description' name='description' placeholder='Describe the grievance you have...'  value={this.state.description} onChange={this.handleChange}/>
-        <Form.Button>Submit</Form.Button>
-      </Form>
+      <div>
+        <Grid>
+          <Grid.Column width={6}>
+            <Search
+              loading={this.state.isLoading}
+              onResultSelect={this.handleResultSelect}
+              onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
+              results={this.state.results}
+              value={this.state.value}
+              resultRenderer={resultRenderer}
+            />
+          </Grid.Column>
+        </Grid>
+
+        <Form onSubmit={this.handleSubmit}>
+          <span>File a Grievance:</span>
+          <Form.Group widths='equal'>
+            <Form.Select fluid onChange={this.handleUserChange} label='Receiver' name='receiver_ids' placeholder='Receiver' options={userOptions}/>
+          </Form.Group>
+          <Form.Input fluid label='Title' placeholder='Title' name='title' onChange={this.handleChange} value={this.state.title}/>
+          <Form.TextArea label='Description' name='description' placeholder='Describe the grievance you have...'  value={this.state.description} onChange={this.handleChange}/>
+          <Form.Button>Submit</Form.Button>
+        </Form>
+      </div>
     )
   }
 }
