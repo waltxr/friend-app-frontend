7dbaebaeeb367277823e7ea2dbdb9c70ba7894a5
Adam Walter <Waltxr@users.noreply.github.com>
2020-02-20 20:00:25 -0500

    Able to search for groups based on name.

--- src/container/users/PublicGroups.js
+++ src/container/users/PublicGroups.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { List, Container, Header, Search } from 'semantic-ui-react'
 import GroupListItem from './GroupListItem'
+import { API_URL } from '../../actions/apiUrl'
+
 
 const initialState = groups =>  {
   return {
@@ -15,15 +17,41 @@ const initialState = groups =>  {
 class PublicGroups extends Component {
   state = initialState(this.props.groups)
 
+  handleSearchChange = (e, { value }) => {
+    this.setState( {isLoading: true, value} )
+
+    setTimeout(() => {
+      fetch((`${API_URL}/groups?filter=${this.state.value}`), {
+        method: 'GET',
+        headers: new Headers({
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${localStorage.token}`,
+        })
+      })
+      .then(res => res.json())
+      .then(filteredGroups => {
+        this.setState( {results: filteredGroups, isLoading: false})
+      })
+    }, 300)
+  }
+
   render() {
-    const groups = this.state.results
+    console.log(this.state);
+    const { isLoading, value, results } = this.state
+
+    const groups = results
     .map( group => <GroupListItem group={group} key= {group.id} /> )
 
     return(
       <Container style={{marginTop: '3em'}}>
         <Header>Join Groups</Header>
-          <Search>
-          </Search>
+          <Search
+              onSearchChange={this.handleSearchChange}
+              loading={isLoading}
+              results={results}
+              value={value}
+              {...this.props}
+          />
           <List animated verticalAlign='middle'>
             {groups}
           </List>
